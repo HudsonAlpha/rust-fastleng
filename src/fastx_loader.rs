a41cff8bd2281b01fc270f5a14d94df1179ec58a
holtjma <jholt@hudsonalpha.org>
2021-08-24 10:28:37 -0500

    adding test files directly, this seems to be the generally support approach

--- src/fastx_loader.rs
+++ src/fastx_loader.rs
@@ -35,24 +35,8 @@ pub fn gather_fastx_stats(filename: &str) -> BTreeMap<usize, u64> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tempfile::{Builder, NamedTempFile};
-    use std::fs::File;
-    use std::io::Write;
     
     /// This one is a single sequence "A"
-    fn create_basic_fasta() -> NamedTempFile {
-        let temp_file: NamedTempFile = Builder::new().prefix("temp_data_").suffix(".fa").tempfile().unwrap();
-        let filename: String = temp_file.path().to_str().unwrap().to_string();
-
-        let mut fasta_file: File = File::create(filename).unwrap();
-        let data_string = b">1
-A";
-        fasta_file.write_all(data_string).unwrap();
-        fasta_file.flush().unwrap();
-
-        temp_file
-    }
-
     fn stats_basic_fasta() -> BTreeMap<usize, u64> {
         let mut results: BTreeMap<usize, u64> = BTreeMap::new();
         results.insert(1, 1);
@@ -60,65 +44,15 @@ A";
     }
 
     /// one of each length from 1-5
-    fn create_basic_fasta2() -> NamedTempFile {
-        let temp_file: NamedTempFile = Builder::new().prefix("temp_data_").suffix(".fa").tempfile().unwrap();
-        let filename: String = temp_file.path().to_str().unwrap().to_string();
-
-        let mut fasta_file: File = File::create(filename).unwrap();
-        let data_string = b">1
-A
->2
-AA
->3
-AAA
->4
-AAAA
->5
-AAAAA";
-        fasta_file.write_all(data_string).unwrap();
-        fasta_file.flush().unwrap();
-
-        temp_file
-    }
-
     fn stats_basic_fasta2() -> BTreeMap<usize, u64> {
         let mut results: BTreeMap<usize, u64> = BTreeMap::new();
-        results.insert(1, 1);
-        results.insert(2, 1);
-        results.insert(3, 1);
-        results.insert(4, 1);
-        results.insert(5, 1);
+        for l in 1..6 {
+            results.insert(l, 1);
+        }
         results
     }
 
     /// mix of a few lengths from 1-4
-    fn create_basic_fasta3() -> NamedTempFile {
-        let temp_file: NamedTempFile = Builder::new().prefix("temp_data_").suffix(".fa").tempfile().unwrap();
-        let filename: String = temp_file.path().to_str().unwrap().to_string();
-
-        let mut fasta_file: File = File::create(filename).unwrap();
-        let data_string = b">1
-AAAA
->2
-AA
->3
-A
->4
-AA
->5
-A
->6
-AAA
->7
-AAAA
->8
-A";
-        fasta_file.write_all(data_string).unwrap();
-        fasta_file.flush().unwrap();
-
-        temp_file
-    }
-
     fn stats_basic_fasta3() -> BTreeMap<usize, u64> {
         let mut results: BTreeMap<usize, u64> = BTreeMap::new();
         results.insert(1, 3);
@@ -131,8 +65,7 @@ A";
     #[test]
     fn test_basic_fasta() {
         //build some inputs
-        let temp_file = create_basic_fasta();
-        let filename = temp_file.path().to_str().unwrap().to_string();
+        let filename = "./test_data/single_string.fa";
 
         //get the expected outputs
         let expected = stats_basic_fasta();
@@ -145,8 +78,7 @@ A";
     #[test]
     fn test_basic_fasta2() {
         //build some inputs
-        let temp_file = create_basic_fasta2();
-        let filename = temp_file.path().to_str().unwrap().to_string();
+        let filename = "./test_data/five_strings.fa";
 
         //get the expected outputs
         let expected = stats_basic_fasta2();
@@ -159,8 +91,7 @@ A";
     #[test]
     fn test_basic_fasta3() {
         //build some inputs
-        let temp_file = create_basic_fasta3();
-        let filename = temp_file.path().to_str().unwrap().to_string();
+        let filename = "./test_data/small_strings.fa";
 
         //get the expected outputs
         let expected = stats_basic_fasta3();
