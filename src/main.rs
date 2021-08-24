45284171d7425e228d216218fee502e39843333e
holtjma <jholt@hudsonalpha.org>
2021-08-24 12:45:54 -0500

    basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 
-//use std::path::PathBuf;
+extern crate clap;
 
-//extern crate needletail;
-//use needletail::parse_fastx_file;
-
+use clap::{App, Arg};
 use std::collections::BTreeMap;
 
 use fastleng::fastx_loader::gather_fastx_stats;
 use fastleng::length_stats::{LengthStats,compute_length_stats};
 
+const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+
 fn main() {
     //let test_file = "/Users/matt/Downloads/test_1M.fq.gz";
-    let test_file = "/Users/matt/Downloads/test_100k_pb.fq.gz";
+    //let test_file = "/Users/matt/Downloads/test_100k_pb.fq.gz";
+
+    let matches = App::new("fastleng")
+        .version(VERSION.unwrap_or("?"))
+        .author("J. Matthew Holt <jholt@hudsonalpha.org>")
+        .about("fastleng - a sequence length statistics generator for fastx files")
+        .arg(Arg::with_name("FASTX")
+            .help("The FASTQ/A file to gather stats on, gzip accepted")
+            .required(true)
+            .index(1))
+        .get_matches();
     
+    let fastx_fn: String = matches.value_of("FASTX").unwrap().to_string();
+
     //load the fastx file lengths
-    let length_counts: BTreeMap<usize, u64> = gather_fastx_stats(&test_file);
+    let length_counts: BTreeMap<usize, u64> = gather_fastx_stats(&fastx_fn);
     
     //compute the stats
     let length_metrics: LengthStats = compute_length_stats(&length_counts);
-    println!("{:?}", length_metrics);
-
     // Serialize it to a JSON string.
-    let j = serde_json::to_string(&length_metrics).unwrap();
-    println!("{}", j);
+    let json_format: String = serde_json::to_string_pretty(&length_metrics).unwrap();
+    println!("{}", json_format);
 }
