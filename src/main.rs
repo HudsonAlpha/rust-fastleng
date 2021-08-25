29c8bb286744bdad3e163a8a2ece2e40574e14bd
holtjma <jholt@hudsonalpha.org>
2021-08-25 08:25:51 -0500

    removing format because it looks like a nightmare

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ extern crate exitcode;
 extern crate log;
 
 use clap::{App, Arg};
-use log::{info, error};
+use log::{error, info};
 use std::collections::BTreeMap;
 use std::fs::File;
 
 use fastleng::fastx_loader::gather_fastx_stats;
-use fastleng::length_stats::{LengthStats,compute_length_stats};
+use fastleng::length_stats::{compute_length_stats, LengthStats};
 
 const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
@@ -22,18 +22,20 @@ fn main() {
         .version(VERSION.unwrap_or("?"))
         .author("J. Matthew Holt <jholt@hudsonalpha.org>")
         .about("fastleng - a sequence length statistics generator for fastx files")
-        .arg(Arg::with_name("FASTX")
-            .help("The FASTQ/A file to gather stats on, gzip accepted")
-            .required(true)
-            .index(1))
+        .arg(
+            Arg::with_name("FASTX")
+                .help("The FASTQ/A file to gather stats on, gzip accepted")
+                .required(true)
+                .index(1)
+        )
         .get_matches();
-    
+
     let fastx_fn: String = matches.value_of("FASTX").unwrap().to_string();
 
     info!("Input parameters (required):");
     info!("\tFASTX: \"{}\"", fastx_fn);
     match File::open(&fastx_fn) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => {
             error!("Failed to open FASTX file: {:?}", fastx_fn);
             error!("Error: {:?}", e);
@@ -50,13 +52,13 @@ fn main() {
             std::process::exit(exitcode::IOERR);
         }
     };
-    
+
     //compute the stats
     let length_metrics: LengthStats = compute_length_stats(&length_counts);
     // Serialize it to a JSON string.
     let json_format: String = serde_json::to_string(&length_metrics).unwrap();
     info!("Length metrics: {}", json_format);
-    
+
     //TODO: this is what we should put in the file
     //let pretty_json: String = serde_json::to_string_pretty(&length_metrics).unwrap();
     
